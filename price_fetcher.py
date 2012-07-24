e3629b524a64b72906b5690cc073174c9458d632
Ryan Moyer <rymoyer@gmail.com>
2012-07-23 21:55:01 -0400

    Fetched item data from Runescape servers.

--- price_fetcher.py
+++ price_fetcher.py
@@ -3,9 +3,12 @@
 
 from __future__ import print_function
 import argparse
+import requests
 
 parser = argparse.ArgumentParser(description='Grabs item ID.')
 parser.add_argument('item_id', metavar='ITEM_ID', type=int, help='the id for an item')
 args = parser.parse_args()
 item_id = args.item_id
-print('http://services.runescape.com/m=itemdb_rs/api/catalogue/detail.json?item={0}'.format(item_id))
+item_url = 'http://services.runescape.com/m=itemdb_rs/api/catalogue/detail.json?item={0}'.format(item_id)
+request = requests.get(item_url)
+print(request.text)
