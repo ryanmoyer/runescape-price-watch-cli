ce0477fdbea11358530e82d3c5ce61552c00d3fa
Ryan Moyer <rymoyer@gmail.com>
2012-07-23 21:39:01 -0400

    Prints url with item id at the end.

--- price_fetcher.py
+++ price_fetcher.py
@@ -7,4 +7,5 @@ import argparse
 parser = argparse.ArgumentParser(description='Grabs item ID.')
 parser.add_argument('item_id', metavar='ITEM_ID', type=int, help='the id for an item')
 args = parser.parse_args()
-print(args.item_id)
+item_id = args.item_id
+print('http://services.runescape.com/m=itemdb_rs/api/catalogue/detail.json?item={0}'.format(item_id))
