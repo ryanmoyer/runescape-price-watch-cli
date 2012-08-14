df71e3e5d98514bc35522b9bb51712e22f5bb199
Ryan Moyer <rymoyer@gmail.com>
2012-08-13 23:07:17 -0400

    Tests for Maple Logs, Fire Runes, and Spirit Shards

--- tests/test_fetcher.py
+++ tests/test_fetcher.py
@@ -3,7 +3,9 @@ import unittest
 import fetcher
 
 class TestFetcher(unittest.TestCase):
-    def test_item_4721(self):
-        self.assertEqual(fetcher.fetch_price(4721), 4721)
-    def test_item_4722(self):
-        self.assertEqual(fetcher.fetch_price(4722), 4722)
+    def test_maple_logs(self):
+        self.assertEqual(fetcher.fetch_price(1517), 51)
+    def test_fire_rune(self):
+        self.assertEqual(fetcher.fetch_price(554), 27)
+    def test_spirit_shards(self):
+        self.assertEqual(fetcher.fetch_price(12183), 24)
