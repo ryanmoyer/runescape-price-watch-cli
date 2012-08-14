2b32c87fd957fb4a969fb66fb5c5d9d1d1732fba
Ryan Moyer <rymoyer@gmail.com>
2012-08-13 23:13:03 -0400

    fetcher and main replace price_fetcher

--- fetcher.py
+++ fetcher.py
@@ -1,2 +1,8 @@
+import requests
+
 def fetch_price(item_id):
-    return item_id
+    item_url = 'http://services.runescape.com/m=itemdb_rs/api/catalogue/detail.json?item={0}'.format(item_id)
+    request = requests.get(item_url)
+    item_inner = request.json['item']
+    item_price = item_inner['current']['price']
+    return item_price
