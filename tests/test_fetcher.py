11da46e32753a65c6785459564940970b2b4898e
Ryan Moyer <rymoyer@gmail.com>
2012-11-01 19:21:50 -0400

    Made test for maple logs independent of the price.

--- tests/test_fetcher.py
+++ tests/test_fetcher.py
@@ -1,11 +1,19 @@
 import unittest
 
+import mock
+
 import fetcher
 
+
+@mock.patch('requests.get')
 class TestFetcher(unittest.TestCase):
-    def test_maple_logs(self):
-        self.assertEqual(fetcher.fetch_price(1517), 51)
-    def test_fire_rune(self):
-        self.assertEqual(fetcher.fetch_price(554), 27)
-    def test_spirit_shards(self):
-        self.assertEqual(fetcher.fetch_price(12183), 24)
+    def test_maple_logs(self, mock_get):
+        mock_response = mock.MagicMock()
+        mock_response.json = {'item': {'current': {'price': 12345}}}
+        mock_get.return_value = mock_response
+        self.assertEqual(fetcher.fetch_price(1517), 12345)
+        mock_get.assert_called_once_with('http://services.runescape.com/m=itemdb_rs/api/catalogue/detail.json?item=1517')
+##    def test_fire_rune(self):
+##        self.assertEqual(fetcher.fetch_price(554), 27)
+##    def test_spirit_shards(self):
+##        self.assertEqual(fetcher.fetch_price(12183), 24)
