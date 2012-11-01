4560e8f0efb0ade34bb31eeb4f1693a1eacc10c2
Ryan Moyer <rymoyer@gmail.com>
2012-11-01 19:03:14 -0400

    Changed the variable request to response.

--- fetcher.py
+++ fetcher.py
@@ -2,7 +2,7 @@ import requests
 
 def fetch_price(item_id):
     item_url = 'http://services.runescape.com/m=itemdb_rs/api/catalogue/detail.json?item={0}'.format(item_id)
-    request = requests.get(item_url)
-    item_inner = request.json['item']
+    response = requests.get(item_url)
+    item_inner = response.json['item']
     item_price = item_inner['current']['price']
     return item_price
