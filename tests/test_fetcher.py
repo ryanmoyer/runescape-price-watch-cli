f6d70ecca97f3a98bfb20009662f247f4ff1c7d7
Ryan Moyer <rymoyer@gmail.com>
2012-11-01 19:46:59 -0400

    Made test for fire rune independent of the price.

--- tests/test_fetcher.py
+++ tests/test_fetcher.py
@@ -1,19 +1,22 @@
 import unittest
 
-import mock
+from mock import MagicMock, patch
 
 import fetcher
 
 
-@mock.patch('requests.get')
+@patch('requests.get')
 class TestFetcher(unittest.TestCase):
     def test_maple_logs(self, mock_get):
-        mock_response = mock.MagicMock()
+        mock_response = MagicMock()
         mock_response.json = {'item': {'current': {'price': 12345}}}
         mock_get.return_value = mock_response
         self.assertEqual(fetcher.fetch_price(1517), 12345)
         mock_get.assert_called_once_with('http://services.runescape.com/m=itemdb_rs/api/catalogue/detail.json?item=1517')
-##    def test_fire_rune(self):
-##        self.assertEqual(fetcher.fetch_price(554), 27)
-##    def test_spirit_shards(self):
-##        self.assertEqual(fetcher.fetch_price(12183), 24)
+
+    def test_fire_rune(self, mock_get):
+        mock_response = MagicMock()
+        mock_response.json = {'item': {'current': {'price': 54321}}}
+        mock_get.return_value = mock_response
+        self.assertEqual(fetcher.fetch_price(554), 54321)
+        mock_get.assert_called_once_with('http://services.runescape.com/m=itemdb_rs/api/catalogue/detail.json?item=554')
