1390dbf55be2f420992d99438de3683f34d1324d
Ryan Moyer <rymoyer@gmail.com>
2012-12-08 15:25:54 -0500

    Fetches multiple item prices using multiple ids at once.

--- main.py
+++ main.py
@@ -2,13 +2,13 @@
 # Author: Ryan Moyer
 
 from __future__ import print_function
-import fetcher
+
+from multi_fetcher import fetch_prices
+
 import argparse
 
 parser = argparse.ArgumentParser(description='Grabs item ID.')
-parser.add_argument('item_id', metavar='ITEM_ID', type=int, help='the id for an item')
+parser.add_argument('item_ids', metavar='ITEM_ID', type=int, nargs='+', help='the id for an item')
 args = parser.parse_args()
-item_id = args.item_id
 
-name, price = fetcher.fetch_price(item_id)
-print('{0}: {1}'.format(name, price))
+print(fetch_prices(args.item_ids))
